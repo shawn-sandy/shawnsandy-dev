8c0f2bfae7f3769446eff3ce218150a28c97b73b
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-03-27 17:05:03 -0400

    updates

--- system/_data/env.js
+++ system/_data/env.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   hello: process.env.HELLO || 'Hello not set, but hi, anyway 👋',
-  node_env: process.env.NODE_ENV || null,
+  node_env: process.env.NODE_ENV || 'development',
   site_map_url: process.env.SITE_MAP_URL || 'https//site.com',
   contact_email: process.env.CONTACT_EMAIL || 'site@amce.inc',
   contact_success: process.env.CONTACT_SUCCESS || '/thank-you',
