40da8666989eacb0595f4ea8493f4792494630d0
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-04-24 16:59:40 -0400

    Task: build updates

--- src/js/form.js
+++ src/js/form.js
@@ -1,14 +1,14 @@
 // contact form/update 05/march/2020
-document.querySelector("form").addEventListener("submit", handleSubmit)
 
 const handleSubmit = (e) => {
   e.preventDefault()
-  let myForm = document.getElementById('contact-us')
-  let formData = new FormData(myForm)
+  const myForm = document.getElementById('contact-us')
+  const formData = new FormData(myForm)
   fetch('/', {
     method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams(formData).toString()
   }).then(() => console.log('Form successfully submitted')).catch((error) =>
     alert(error))
 }
+document.querySelector('form').addEventListener('submit', handleSubmit)
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// @ts-check
 const msg = 'development mode active, please remove the devMode() function in ./js/scripts/index.js and replace with your own js functions, thank you and enjoy.'
 
 const devMode = () => {
