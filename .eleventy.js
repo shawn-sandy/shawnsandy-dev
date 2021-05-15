91a43c429c8bf65255e11c9af621e50a6afa2a25
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-05-15 04:51:42 -0400

    Task: add/config eshint

--- .eleventy.js
+++ .eleventy.js
@@ -1,66 +1,63 @@
-const fs = require("fs")
+// @ts-check
+const fs = require("fs");
 
-const htmlmin = require("html-minifier")
-const pluginRss = require("@11ty/eleventy-plugin-rss")
-const markdown = require("@shawnsandy/ideas/lib/markdown")
-const image = require("@11ty/eleventy-img")
-const CleanCSS = require("clean-css")
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
-const take = ("lodash.take")
+const htmlmin = require("html-minifier");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const markdown = require("@shawnsandy/ideas/lib/markdown");
+const image = require("@11ty/eleventy-img");
+const CleanCSS = require("clean-css");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const take = "lodash.take";
 
 module.exports = function (eleventyConfig) {
-
   eleventyConfig.setBrowserSyncConfig({
     notify: true,
     open: true,
     callbacks: {
       ready: function (err, bs) {
         bs.addMiddleware("*", (req, res) => {
-          const content_404 = fs.readFileSync("www/404.html")
-          // Provides the 404 content without redirect.
-          res.write(content_404)
-          // Add 404 http status code in request header.
-          // res.writeHead(404, { "Content-Type": "text/html" });
-          res.writeHead(404)
-          res.end()
-        })
+          const content_404 = fs.readFileSync("www/404.html");
+          res.write(content_404);
+          res.writeHead(404);
+          res.end();
+        });
       },
     },
-  })
+  });
 
-  eleventyConfig.setQuietMode(true)
+  eleventyConfig.setQuietMode(true);
 
-  eleventyConfig.addWatchTarget("./src/sass/")
-  eleventyConfig.addWatchTarget("./README.md")
-  eleventyConfig.addWatchTarget("./src/js/")
-  eleventyConfig.addPassthroughCopy("./src/css", "www/css/")
-  eleventyConfig.addPassthroughCopy("./system/dsp/assets", "www/assets/")
-  eleventyConfig.addPassthroughCopy("./src/images", "./images/")
-  eleventyConfig.addPassthroughCopy("./img")
-  eleventyConfig.addPassthroughCopy("./src/js/min", "www/js")
-  eleventyConfig.addPassthroughCopy("./src/admin")
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addWatchTarget("./src/js/");
+  eleventyConfig.addPassthroughCopy("./src/css", "www/css/");
+  eleventyConfig.addPassthroughCopy("./system/dsp/assets", "www/assets/");
+  eleventyConfig.addPassthroughCopy("./src/images", "./images/");
+  eleventyConfig.addPassthroughCopy("./img");
+  eleventyConfig.addPassthroughCopy("./src/js/min", "www/js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   // eleventyConfig.addPassthroughCopy("./src/robots.txt")
 
   // eleventy img
   //
-  eleventyConfig.addNunjucksAsyncShortcode("img", async function (src, alt = "", outputFormat = "jpeg") {
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "img",
+    async function (src, alt = "", outputFormat = "jpeg") {
+      let stats = await image(src, {
+        widths: [null],
+        formats: [outputFormat],
+        urlPath: "/images/",
+        outputDir: "./www/images/",
+      });
+      let props = stats[outputFormat].pop();
 
-    let stats = await image(src, {
-      widths: [null],
-      formats: [outputFormat],
-      urlPath: "/images/",
-      outputDir: "./www/images/"
-    })
-    let props = stats[outputFormat].pop()
-
-    return `<img src="${props.url}" alt="${alt}">`
-  })
+      return `<img src="${props.url}" alt="${alt}">`;
+    }
+  );
 
   // css-min filter
   eleventyConfig.addFilter("cssmin", (code) => {
-    return new CleanCSS({}).minify(code).styles
-  })
-
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   //Minify our HTML
   eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
@@ -69,28 +66,28 @@ module.exports = function (eleventyConfig) {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
-      })
-      return minified
+      });
+      return minified;
     }
-    return content
-  })
+    return content;
+  });
 
   eleventyConfig.addCollection("developer", (collection) =>
     collection.getFilteredByTags("developers").sort((a, b) => {
-      if (a.data.page.fileSlug > b.data.page.fileSlug) return -1
-      else if (a.data.page.fileSlug < b.data.page.fileSlug) return 1
-      else return 0
+      if (a.data.page.fileSlug > b.data.page.fileSlug) return -1;
+      else if (a.data.page.fileSlug < b.data.page.fileSlug) return 1;
+      else return 0;
     })
-  )
+  );
 
-  eleventyConfig.addPlugin(require("@shawnsandy/ideas/eleventy"))
-  eleventyConfig.addPlugin(require("@shawnsandy/code-clipper"))
+  eleventyConfig.addPlugin(require("@shawnsandy/ideas/eleventy"));
+  eleventyConfig.addPlugin(require("@shawnsandy/code-clipper"));
 
-  eleventyConfig.addPlugin(pluginRss)
-  eleventyConfig.addPlugin(markdown)
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(markdown);
 
   // navigation
-  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
     dir: {
@@ -101,7 +98,7 @@ module.exports = function (eleventyConfig) {
     },
     templateFormats: ["njk", "html", "md"],
     htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk"
+    markdownTemplateEngine: "njk",
     // pathPrefix: "/",
-  }
-}
+  };
+};
