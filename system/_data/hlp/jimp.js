3886b1a361d8539c3f9219dd68670a4ff4b20f9f
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-05-29 00:35:02 -0400

    feat: Add check file exist check on jimp

--- system/_data/hlp/jimp.js
+++ system/_data/hlp/jimp.js
@@ -1,24 +1,36 @@
 // @ts-check
 
 const Jimp = require('jimp')
+const fs = require('fs')
 
-const img = ($img = 'aem-pr-request.jpg', $options = {}) => {
-  const width = $options.width || 1280
-  const height = $options.width || Jimp.AUTO
-  const srcDir = $options.srcDir || './img/'
-  const outputDir = $options.outputDir || './www/images/'
-  const outputCache = $options.outputDir || './images/min/'
+const img = ($img = 'google-security-check.jpg', options = {}) => {
+  const width = options.width || 1280
+  const height = options.width || Jimp.AUTO
+  const srcDir = options.srcDir || './img/'
+  const outputDir = options.outputDir || './www/images/'
+  const outputCache = options.outputDir || './images/min/'
 
-  Jimp.read(`${srcDir}${$img}`, (err, lenna) => {
-    if (err) throw err
-    lenna
-      .resize(width, height)
-      .quality(60)
-      .write(`${outputDir}${$img}`)
-      .write(`${outputCache}${$img}`)
-    // console.log(lenna)
-  })
-  return `/images/${$img}`
+  const imagePath = `${outputCache}${$img}`
+
+  try {
+    if (!fs.existsSync(imagePath)) {
+      Jimp.read(`${srcDir}${$img}`, (err, lenna) => {
+        if (err) throw err
+        lenna
+          .resize(width, height)
+          .quality(60)
+          .write(`${outputDir}${$img}`)
+          .write(`${outputCache}${$img}`)
+        console.log(lenna)
+      })
+    } else {
+      console.log(`Image already exist ${imagePath}`)
+    }
+
+    return `/images/${$img}`
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = {
