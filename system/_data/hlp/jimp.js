dd536466274aac3096c43c4b65bc40c3f04c4385
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-05-30 20:58:33 -0400

    Develop (#25)

--- system/_data/hlp/jimp.js
+++ system/_data/hlp/jimp.js
@@ -1,24 +1,39 @@
 // @ts-check
 
 const Jimp = require('jimp')
+const fs = require('fs')
 
-const img = ($img = 'aem-pr-request.jpg', $options = {}) => {
-  const width = $options.width || 1280
-  const height = $options.width || Jimp.AUTO
-  const srcDir = $options.srcDir || './img/'
-  const outputDir = $options.outputDir || './www/images/'
-  const outputCache = $options.outputDir || './images/min/'
+const img = (imgs = 'google-security-check.jpg', name = null, options = {}) => {
+  const width = options.width || 1280
+  const fileName = name || `data-${Date.now()}`
+  const height = options.width || Jimp.AUTO
+  const srcDir = options.srcDir || './assets/img/'
+  const outputDir = options.outputDir || './www/images/'
+  const quality = options.quality || 80
+  const imageSrc = `${srcDir}${imgs}`
+  const imagePath = `${outputDir}${imgs}`
+  const force = options.force || false
+  const outputCache = options.cacheDir || './.cache/images/'
 
-  Jimp.read(`${srcDir}${$img}`, (err, lenna) => {
-    if (err) throw err
-    lenna
-      .resize(width, height)
-      .quality(60)
-      .write(`${outputDir}${$img}`)
-      .write(`${outputCache}${$img}`)
-    // console.log(lenna)
-  })
-  return `./images/${$img}`
+  try {
+    if (!fs.existsSync(`${imagePath}`) && fs.existsSync(imageSrc) || force) {
+      console.log('processing image')
+
+      Jimp.read(`${srcDir}${imgs}`, (err, lenna) => {
+        if (err) throw err
+        lenna
+          .resize(width, height)
+          .quality(quality)
+          .write(`${outputDir}${imgs}`)
+          .write(`${outputCache}${imgs}`)
+      })
+    } else {
+      console.log(`Seems image already exist ${imagePath}`)
+    }
+  } catch (error) {
+    console.info(error)
+  }
+  return `/images/${imgs}`
 }
 
 module.exports = {
---
+++ system/_data/hlp/sharp.js
@@ -0,0 +1,40 @@
+
+const sharp = require('sharp')
+const fs = require('fs')
+
+const img = (imgs = 'google-security-check.jpg', options = {}) => {
+  const _name = imgs.split('.')
+  const imgName = options.name || _name[0].replace('/', '-')
+  const width = options.width || 1280
+  const format = options.format || 'webp'
+  const height = options.width || null
+  const srcDir = options.srcDir || './assets/img/'
+  const outputDir = options.outputDir || './www/images/'
+  const quality = options.quality || 80
+  const imageSrc = `${srcDir}${imgs}`
+  const imagePath = `${outputDir}${imgName}.${format}`
+  const force = options.force || false
+  const outputCache = options.cacheDir || './.cache/images/'
+
+  if (!fs.existsSync(imagePath) || force) {
+    console.warn('Image processing')
+    sharp(`${imageSrc}`)
+      .toFormat(format)
+      .resize({ width: width, height: height })
+      .toFile(`${outputDir}${imgName}.${format}`, (err) => {
+        console.log('Error', err)
+      })
+  } else {
+    console.warn('Image exists')
+  }
+  return `/images/${imgName}.${format}`
+}
+
+const imgSrc = (imgs = 'google-security-check.jpg', name = null, options = {}) => {
+  return `<img src="${img(imgs, name, options)}" alt="${options.name || name}" />`
+}
+
+module.exports = {
+  img,
+  imgSrc
+}
