4098bc354956ef4e1e21f5e8dcf10b54fdaec9ae
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-06-04 07:22:56 -0400

    refactor: remove duplicate options, add conditionals check
    - conditional error checking for betted developer experience \\n

--- system/_data/hlp/sharp.js
+++ system/_data/hlp/sharp.js
@@ -8,10 +8,14 @@ const getName = (img) => {
   return imgName
 }
 
+/**
+ *
+ * @param {*} imgs
+ * @param {*} options
+ * @returns
+ */
 const resize = (imgs = [], options = {}) => {
-  const width = options.width || 1280
   const format = options.format || 'jpeg'
-  const height = options.width || null
   const srcDir = options.srcDir || './assets/img/'
   const outputDir = options.outputDir || './www/images/'
   const imageSrc = `${srcDir}${imgs}`
@@ -32,7 +36,7 @@ const resize = (imgs = [], options = {}) => {
       console.warn(imgName)
       sharp(`${imageSrc}`)
         .toFormat(format)
-        .resize({ width: width, height: height })
+        .resize({ width: options.width || null, height: options.width || null })
         .toFile(`${outputDir}${imgName}.${format}`, (err, info) => {
           if (err) console.log('Error', err)
           // if (info) console.warn('Info', info)
@@ -44,6 +48,11 @@ const resize = (imgs = [], options = {}) => {
 }
 
 const img = (imgs = 'google-security-check.jpg', options = {}) => {
+  if (!Array.isArray(imgs)) {
+    console.error('Image param needs to be array')
+    return
+  }
+
   if (imgs.length > 0) {
     resize([imgs[0]], options)
     const imgName = getName(imgs[0])
@@ -52,8 +61,8 @@ const img = (imgs = 'google-security-check.jpg', options = {}) => {
   return null
 }
 
-const imgSrc = (imgs = 'google-security-check.jpg', name = null, options = {}) => {
-  return `<img src="${img(imgs, name, options)}" alt="${options.name || name}" />`
+const imgSrc = (imgs = 'google-security-check.jpg', options = {}) => {
+  return `<img src="${img(imgs, options)}" alt="${options.name}"/>`
 }
 
 module.exports = {
