541c994573d17f8c702530ac5a448c1156267af1
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2021-06-04 06:48:49 -0400

    refactor: remove devmode warning in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 // @ts-check
-const msg = 'development mode active, please remove the devMode() function in ./js/scripts/index.js and replace with your own js functions, thank you and enjoy.'
+// const msg = 'development mode active, please remove the devMode() function in ./js/scripts/index.js and replace with your own js functions, thank you and enjoy.'
+// const
+//  devMode = () => {
+//   console.warn(`Hello and Welcome, ${msg}`)
+// }
 
-const devMode = () => {
-  console.warn(`Hello and Welcome, ${msg}`)
-}
-
-devMode()
+// devMode()
